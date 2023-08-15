d1e29f7eae66edba341ca5b0c1112356a1586bae
maxellmilay <milaymaxell@gmail.com>
2023-08-16 01:23:05 +0800

    [Adjustment][Maxell] added semantic tags

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -2,27 +2,24 @@
   <div class="flex flex-col w-[90%] py-12 md:h-full">
     <h2 class="font-karla text-white text-4xl text-center md:text-left">Dashboard</h2>
     <div class="flex flex-col md:flex-row grow gap-10 py-10">
-      <div class="flex flex-col items-center md:h-full gap-y-12">
+      <section class="flex flex-col items-center md:h-full gap-y-12">
         <SummaryItem :icon="SunIcon" :name="'Balance'" :value="3000" />
         <SummaryItem :icon="ShoppingCartIcon" :name="'Expense'" :value="2500" />
         <SummaryItem :icon="CurrencyDollarIcon" :name="'Income'" :value="5500" />
-      </div>
-      <div class="flex flex-col md:h-full md:grow h-[30rem] px-[10%] md:px-0">
-        <h2 class="font-karla text-2xl text-center md:text-left mb-5">Transactions</h2>
+      </section>
+      <section class="flex flex-col md:h-full md:grow h-[30rem] px-[10%] md:px-0">
+        <h3 class="font-karla text-2xl text-center md:text-left mb-5">Transactions</h3>
         <div
           class="flex flex-col basis-0 pr-5 gap-5 grow overflow-y-scroll scrollbar-thumb-white scrollbar-track-black/70 scrollbar-thin"
         >
-          <TransactionItem
-            :amount="2500"
-            description="Ruel Allowanceasdsadas adas dasdad sadasdas dsa dsad"
-          />
+          <TransactionItem :amount="2500" description="Ruel Allowance" />
           <TransactionItem :amount="-2000" description="Samsung Phone Payment" />
           <TransactionItem :amount="2500" description="Ruel Allowance" />
           <TransactionItem :amount="-2000" description="Samsung Phone Payment" />
           <TransactionItem :amount="2500" description="Ruel Allowance" />
           <TransactionItem :amount="-2000" description="Samsung Phone Payment" />
         </div>
-      </div>
+      </section>
     </div>
   </div>
 </template>
--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -3,11 +3,11 @@
     class="bg-site-gray rounded-full aspect-1 w-[12rem] h-[12rem] border-[3px] border-site-primary hover:scale-110 duration-200"
   ></div>
   <h2 class="font-karla text-3xl">Maxell Milay</h2>
-  <div class="flex flex-col gap-5">
+  <section class="flex flex-col gap-5">
     <ProfileSummaryItem name="Balance" :value="3000" />
     <ProfileSummaryItem name="Expenses" :value="2500" />
     <ProfileSummaryItem name="Income" :value="5500" />
-  </div>
+  </section>
 </template>
 
 <script setup lang="ts">
