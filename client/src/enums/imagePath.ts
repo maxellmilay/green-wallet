ba826f8951ee0038141a93f52832a9b6497fa503
maxellmilay <milaymaxell@gmail.com>
2023-08-20 21:40:51 +0800

    [Adjustment][Maxell] added default profile picture

--- client/src/enums/imagePath.ts
+++ client/src/enums/imagePath.ts
@@ -1,6 +1,7 @@
 enum ImagePath {
   MENU_ICON = '/images/menu-icon.png',
   LOGIN_LOGO = '/images/login-logo.png',
+  DEFAULT_PROFILE_PICTURE = '/images/default-profile-picture.jpg',
 }
 
 export default ImagePath;
--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,7 +1,9 @@
 <template>
   <div
-    class="bg-site-gray rounded-full aspect-1 w-[12rem] h-[12rem] border-[3px] border-site-primary hover:scale-110 duration-200"
-  ></div>
+    class="bg-site-gray rounded-full aspect-1 w-[12rem] h-[12rem] border-[3px] border-site-primary hover:scale-110 duration-200 overflow-hidden"
+  >
+    <img :src="ImagePath.DEFAULT_PROFILE_PICTURE" alt="Profile Picture" />
+  </div>
   <h2 class="font-karla text-3xl">{{ name }}</h2>
   <section class="flex flex-col gap-5">
     <ProfileSummaryItem :name="Summary.BALANCE" :value="balance" />
@@ -14,6 +16,7 @@
 import ProfileSummaryItem from '../components/ProfileSummaryItem.vue';
 import Summary from '../enums/summary';
 import mockData from '../mockData';
+import ImagePath from '../enums/imagePath';
 
 const { balance, expenses, income } = mockData.user.data;
 const { name } = mockData.user;
