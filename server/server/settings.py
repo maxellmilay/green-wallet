a84fc90a9abd4cdeca1cd9815ad6a881e0592d98
maxellmilay <milaymaxell@gmail.com>
2023-09-30 05:20:46 +0800

    [Fix][Maxell] removed settings db connection config time

--- server/server/settings.py
+++ server/server/settings.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 # PostgreSQL
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://maxell:vuwtoqr8ed7tYMvPmiKd6SXslX1IkUac@dpg-ckb24vusmu8c73a4l1vg-a.oregon-postgres.render.com/greenwallet',conn_max_age=600)
+    'default': dj_database_url.config(default='postgres://maxell:vuwtoqr8ed7tYMvPmiKd6SXslX1IkUac@dpg-ckb24vusmu8c73a4l1vg-a.oregon-postgres.render.com/greenwallet')
 }
 
 # Password validation
